2f7777b7fd2e4db1fec8a960008b32796250351f
Dariens97 <102767997+Dariens97@users.noreply.github.com>
2025-06-01 19:05:44 -0400

    cambio de tamaño logo

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function Navigation() {
         <Link className="navbar-brand d-flex align-items-center" to="/">
           
           <div className="d-none d-md-block text-start"style={{float:"left"}}>
-            <img src={logo} alt="Logo" width="120" className="me-2" />
+            <img src={logo} alt="Logo" width="100" className="me-2" />
             
             <div>
               <small className="gris">Soluciones Tecnológicas a Tu Medida</small>
