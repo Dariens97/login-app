8cfe18afb52b829ceb15da1846d5275c22d9721b
Dariens97 <102767997+Dariens97@users.noreply.github.com>
2025-06-15 22:21:59 -0400

    cambio en 'nosotros, encabezado, camaras comuntiarias y footer'

--- src/App.js
+++ src/App.js
@@ -11,38 +11,12 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function Navigation() {
   return (
-/*    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">*/
-      <nav className="navbar navbar-expand-lg navbar-dark navbar-gradient">
-
-      <div className="container-fluid" style={{textAlign: 'center' }}>
-
-      {/*Logo e información 
-      <Link className="navbar-brand d-flex align-items-center" to="/">
-        <img src={logo} alt="Logo" width="150" className="me-2" />
-        <h6 className="gris text-center">soluciones tecnologicas</h6>
-      </Link>
-      <Link className="navbar-brand text-center" to="/"><h2>NET-CE</h2>
-      <h5 className="gris text-center">Soluciones Tecnologicas Echas a Tú Medida</h5></Link>
-      */}
-      
-        {/* Logo e información */}
-        <Link className="navbar-brand d-flex align-items-center w-100 justify-content-between" to="/">
-          
-          <div className="d-none d-md-block ms-5">
-            <img src={logo} alt="Logo" width="100" className="me-2" />
-            
-
-          </div>
-          <div className="d-none d-md-block mx-auto " style={{ maxWidth: "800px", fontSize: "0.9rem" }}>
-            <h5 style={{fontSize: "1rem"}}>
-              Tenemos como idea pragmática el concepto “innovación” en función de las personas, 
-              <br></br>
-              estamos constantemente cuestionando nuestro; modelo de negocio, tecnologías, 
-              <br></br>
-              capacidad adaptativa y nivel de empatía hacia nuestro entorno
-            </h5>
-          </div>
-          
+    <nav className="navbar navbar-expand-lg navbar-dark navbar-gradient">
+      <div className="container-fluid px-4">
+        {/* Logo alineado a la izquierda */}
+        <Link className="navbar-brand d-flex align-items-center" to="/" style={{ marginLeft: "20rem" }}>
+          <img src={logo} alt="Logo" width="100" className="me-2" />
+          <small className="gris mb-0">Soluciones <br></br>Tecnológicas</small>
         </Link>
 
         {/* Botón hamburguesa para móviles */}
@@ -58,26 +32,25 @@ function Navigation() {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-
-        {/*Menú colapsable */}
-        <div className="collapse navbar-collapse" id="navbarContent">
-          <ul className="navbar-nav ms-auto text-center">
-            <li className="nav-item">
+        {/* Menú alineado a la derecha, pero no tan al borde */}
+        <div className="collapse navbar-collapse justify-content-end" id="navbarContent">
+          <ul className="navbar-nav" style={{ marginRight: '17rem' }}>
+            <li className="nav-item px-2">
               <Link className="nav-link fs-5" to="/">Inicio</Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item px-2">
               <Link className="nav-link fs-5" to="/about">Nosotros</Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item px-2">
               <Link className="nav-link fs-5" to="/contact">Contacto</Link>
             </li>
           </ul>
         </div>
       </div>
-      </nav>
-    /*</nav>*/
+    </nav>
   );
 }
+
 
 
 
--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,18 @@ import React from 'react';
 function Footer() {
   return (
     <footer style={{ background: '#222', color: '#fff', padding: '2rem', textAlign: 'center' }}>
+      <h5 style={{fontSize: "1rem"}}>
+        Tenemos como idea pragmática el concepto “innovación” en función de las personas, 
+        <br></br>
+        estamos constantemente cuestionando nuestro; modelo de negocio, tecnologías, 
+        <br></br>
+        capacidad adaptativa y nivel de empatía hacia nuestro entorno
+      </h5>
+      <br></br>
       <p>© 2025 NET-CE | Todos los derechos reservados.</p>
+      
+         
+      
     </footer>
   );
 }
