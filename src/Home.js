72c1eecf9f93f1ffd31fe46b66541f0532252a63
Dariens97 <102767997+Dariens97@users.noreply.github.com>
2025-07-13 21:31:04 -0400

    cambio en los textos de la tarjeta soluciones wifi a WIRELESS junto con el contenido, se agregaron iconos a las tarjetas

--- src/Home.js
+++ src/Home.js
@@ -267,75 +267,98 @@ const imagenes = [
             <h4>Gestión de Incidencias</h4>
           </div>
           <div className="col-md-3 bg-light p-4 rounded shadow" style={{ fontSize: '1.1rem',}}>
-            <h5>Asistencia</h5>
-            <p className="fuente-personalizada" >Atención oportuna para reducir el impacto en las operaciones de su negocio. Por este servicio podremos restablecer lo antes posible la operatividad. Para ello contamos con dos niveles de asistencia.
-              el nivel 1: registra, categoriza diagnostica y da soluciones a requerimientos más recurrentes.
-              <br/>
-              caso contrario pasa a nivel 2 quienes investigan y resuelven. para ello se implementó una plataforma de administración de asistencias (TICKETS)
-              </p>
+            <h5>Asistencia <br/> <img src={`${process.env.PUBLIC_URL}/img/Asistencia.jpg`} alt="asistencia" style={{ height: '50px', margin: '10px' }} /></h5>
+              <div style={{ textAlign: "justify"}}>
+                <p className="fuente-personalizada" >Atención oportuna para reducir el impacto en las operaciones de su negocio. Por este servicio podremos restablecer lo antes posible la operatividad. Para ello contamos con dos niveles de asistencia.
+                  el nivel 1: registra, categoriza diagnostica y da soluciones a requerimientos más recurrentes.
+                  <br/>
+                  caso contrario pasa a nivel 2 quienes investigan y resuelven. para ello se implementó una plataforma de administración de asistencias (TICKETS)
+                </p>
+              </div>
           </div>
           <div className="col-md-3 bg-light p-4 rounded shadow" style={{ fontSize: '1.1rem',}}>
-            <h5>Servicio Preventivo</h5>
-            <p className="fuente-personalizada" >Anticipación a fallas, es crucial para su continuidad operativa. El objetivo de un contrato de mantenimiento preventivo es aumentar la durabilidad de tu equipamiento tecnologico, previniendo desperfectos y minimizando fallas
-              </p>
+            <h5>Servicio Preventivo <br/><img src={`${process.env.PUBLIC_URL}/img/Preventivo.png`} alt="preventivo" style={{ height: '50px', margin: '10px' }} /></h5>
+             <div style={{ textAlign: "justify"}}>
+                <p className="fuente-personalizada" >Anticipación a fallas, es crucial para su continuidad operativa. El objetivo de un contrato de mantenimiento preventivo es aumentar la durabilidad de tu equipamiento tecnologico, previniendo desperfectos y minimizando fallas
+                </p>
+              </div>
           </div>
           <div className="col-md-3 bg-light p-4 rounded shadow" style={{ fontSize: '1.1rem',}}>
-            <h5>Servicio por Eventos</h5>
-            <p className="fuente-personalizada" >Restauración de fallas inesperadas por efectos de la vida util u otras variables que provoquen que el equipo deje de funcionar</p>
+            <h5>Servicio por Eventos <br/><img src={`${process.env.PUBLIC_URL}/img/Eventos.png`} alt="eventos" style={{ height: '50px', margin: '10px' }} /></h5>
+             <div style={{ textAlign: "justify"}}>
+              <p className="fuente-personalizada" >Restauración de fallas inesperadas por efectos de la vida util u otras variables que provoquen que el equipo deje de funcionar</p>
+            </div>
           </div>
         </div>
 
-        <div className="container mt-1 pt-5 row  justify-content-between">
+        <div className="container mt-1 pt-5 row text-center justify-content-between">
           <div style={{ paddingBottom: '50px' }}>
             <h4>Networking</h4>
           </div>
-          <div className="col-md-3 bg-light p-2 rounded shadow " style={{ fontSize: '1.1rem', textAlign: "justify"}}>
-            <h5>Diseño de Redes LAN</h5>
-            <p className="fuente-personalizada " >Trabajamos junto a usted para definir los requerimientos y diseñar una red escalable, adaptada a su entorno. La infraestructura se implementará con tecnología y equipamiento adecuados, incorporando: <br></br>
-                  
-                  - Servidores ISE<br />
-                  - Plataforma Cisco Prime<br />
-                  - Configuración de Firewall y Appliance<br />
-                  - Políticas de calidad de servicio (QoS)<br />
-                  - Administración de redes LAN
-                  </p>
+          <div className="col-md-3 bg-light p-2 rounded shadow " style={{ fontSize: '1.1rem'}}>
+            <h5>Diseño de Redes LAN <br/><img src={`${process.env.PUBLIC_URL}/img/network.png`} alt="network" style={{ height: '50px', margin: '10px' }} /></h5>
+            <div style={{ textAlign: "justify"}}>
+              <p className="fuente-personalizada " >Trabajamos junto a usted para definir los requerimientos y diseñar una red escalable, adaptada a su entorno. La infraestructura se implementará con tecnología y equipamiento adecuados, incorporando: <br></br>
+                    
+                    - Servidores ISE<br />
+                    - Plataforma Cisco Prime<br />
+                    - Configuración de Firewall y Appliance<br />
+                    - Políticas de calidad de servicio (QoS)<br />
+                    - Administración de redes LAN
+              </p>
+            </div>
           </div>
-          <div className="col-md-3 bg-light p-2 rounded shadow" style={{ fontSize: '1.1rem', textAlign: "justify"}}>
-            <h5>Soluciones WiFi</h5>
-            <p className="fuente-personalizada" >Ofrecemos una solución integral para su red inalámbrica, que incluye:
-                    Análisis de cobertura, dimensionamiento y detección de vulnerabilidades
-                    Configuración y administración de tecnología WLC mediante AP ligeros o redes con AP autónomos
-                    Implementación completa de redes WiFi</p>
+          <div className="col-md-3 bg-light p-2 rounded shadow" style={{ fontSize: '1.1rem'}}>
+            <h5>wireless <br/><img src={`${process.env.PUBLIC_URL}/img/wireless.png`} alt="wireless" style={{ height: '50px', margin: '10px' }} /></h5>
+            <div style={{ textAlign: "justify"}}>
+              <p className="fuente-personalizada" >Ofrecemos soluciones de conectividad para su diversidad de dispositivos inalámbricos asegurando; Robustez,
+                seguridad y escalabilidad en la transmisión de los datos
+                <br/>
+                <br/>
+                -Análisis de coberturas<br/>
+                -Detección de vulnerabilidades<br/>
+                -Tecnología WLC (Wireless LAN Controller)<br/>
+                -Implementación wifi</p>
+            </div>
           </div>
-          <div className="col-md-3 bg-light p-2 rounded shadow" style={{ fontSize: '1.1rem',textAlign: "justify"}}>
+          <div className="col-md-3 bg-light p-2 rounded shadow" style={{ fontSize: '1.1rem'}}>
             <h5>Cableado Estructurado</h5>
             <p className="fuente-personalizada" >Realizamos el levantamiento, diseño y proyección del cableado estructurado, además del mantenimiento preventivo y correctivo para asegurar un rendimiento óptimo del sistema.</p>
           </div>
         </div>
 
-        <div className="container mt-5 pt-5 text-center">
+        <div className="container mt-5 pt-4 text-center">
           <h4>Cámaras y Alarmas</h4>
           <div className="row mt-4">
             
             
             <div className="row justify-content-center mt-4">
-              <div className="col-md-5 bg-light p-4 m-2 rounded shadow" style={{ fontSize: '1.1rem', textAlign: "justify"}}>
-                <h5>Inside</h5>
-                <p className="fuente-personalizada">
-                  Brinde tranquilidad a su familia con un sistema de seguridad inalámbrico y de tecnología avanzada. 
-                  <br></br>
-                   <br></br>
-                  Reciba alertas de intrusión directamente en su teléfono, acompañadas de imágenes en tiempo real.
-                </p>
+              <div className="col-md-5 bg-light p-4 m-2 rounded shadow" style={{ fontSize: '1.1rem'}}>
+                <h5>Inside <br/><img src={`${process.env.PUBLIC_URL}/img/camara-inside.png`} alt="camaraInside" style={{ height: '50px', margin: '10px' }} /></h5>
+                <div style={{ textAlign: "justify"}}>
+                    <p className="fuente-personalizada">
+                    Brinde tranquilidad a su familia con un sistema de seguridad inalámbrico y de tecnología avanzada. 
+                    <br></br>
+                    <br></br>
+                    Reciba alertas de intrusión directamente en su teléfono, acompañadas de imágenes en tiempo real.
+                  </p>
+
+                </div>
+                  
               </div>
-              <div className="col-md-5 bg-light p-4 m-2 rounded shadow" style={{ fontSize: '1.1rem', textAlign: "justify"}}>
-                <h5>Perimetral</h5>
-                <p className="fuente-personalizada">
-                  Proteja sus bienes con soluciones tecnológicas que disuaden robos y actos vandálicos. 
-                  <br></br>
-                   <br></br>
-                  Nuestra tecnología perimetral ayuda a reducir los riesgos tanto para su patrimonio como para sus colaboradores.
-                </p>
+              <div className="col-md-5 bg-light p-4 m-2 rounded shadow" style={{ fontSize: '1.1rem'}}>
+                
+                <h5>Perimetral <br/>
+                  <img src={`${process.env.PUBLIC_URL}/img/perimetral2.png`} alt="perimetral" style={{ height: '50px', margin: '10px' }} /></h5>
+                
+                <div style={{ textAlign: "justify"}}>
+                  <p className="fuente-personalizada">
+                    Proteja sus bienes con soluciones tecnológicas que disuaden robos y actos vandálicos. 
+                    <br></br>
+                    <br></br>
+                    Nuestra tecnología perimetral ayuda a reducir los riesgos tanto para su patrimonio como para sus colaboradores.
+                  </p>
+                </div>
               </div>
             </div>
           
@@ -345,7 +368,8 @@ const imagenes = [
               </div>
               <div className="col-md-5 bg-light p-4 m-2 rounded shadow" style={{ fontSize: '1.1rem',}}>  
                 <div>
-                   <h5 >Alarmas comunitarias</h5>
+                   <h5 >Alarmas comunitarias <br/>
+                     <img src={`${process.env.PUBLIC_URL}/img/alarma-comunitaria.png`} alt="alarmacom" style={{ height: '50px', margin: '10px' }} /></h5>
                 </div>
                 <div style={{ textAlign: "justify"}}>
                   <p className="fuente-personalizada">
@@ -359,11 +383,14 @@ const imagenes = [
               </div>
                 
               </div>
-              <div className="col-md-5 bg-light p-4 m-2 rounded shadow" style={{ fontSize: '1.1rem',textAlign: "justify"}}>
-                <h5>Mantención de Equipos</h5>
-                <p className="fuente-personalizada">
-                  Nuestro personal calificado realiza mantenciones periódicas para garantizar que su sistema de seguridad funcione siempre en condiciones óptimas.
-                </p>
+              <div className="col-md-5 bg-light p-4 m-2 rounded shadow" style={{ fontSize: '1.1rem'}}>
+                <h5>Mantención de Equipos <br/>
+                 <img src={`${process.env.PUBLIC_URL}/img/mantencion-camaras.png`} alt="mantcamaras" style={{ height: '50px', margin: '10px' }} /></h5>
+                <div style={{ textAlign: "justify"}}>
+                  <p className="fuente-personalizada">
+                    Nuestro personal calificado realiza mantenciones periódicas para garantizar que su sistema de seguridad funcione siempre en condiciones óptimas.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
@@ -556,18 +583,18 @@ const imagenes = [
 
             
             <div className="d-flex justify-content-around align-items-center flex-wrap">
-              <img src="/img/marcas/hikvision.png" alt="Hikvision" style={{ height: '50px', margin: '10px' }} />
-              <img src="/img/marcas/dahua.png" alt="Dahua" style={{ height: '50px', margin: '10px' }} />
-              <img src="/img/marcas/tplink.png" alt="Ezviz" style={{ height: '50px', margin: '10px' }} />
-              <img src="/img/marcas/tplink.png" alt="Tapo" style={{ height: '50px', margin: '10px' }} />
+              <img src={`${process.env.PUBLIC_URL}/img/hikvision.jpg`} alt="Hikvision" style={{ height: '50px', margin: '10px' }} />
+              <img src={`${process.env.PUBLIC_URL}/img/marcas/dahua.jpg`} alt="Dahua" style={{ height: '25px', margin: '10px' }} />
+              <img src={`${process.env.PUBLIC_URL}/img/marcas/ezviz.jpg`} alt="Ezviz" style={{ height: '65px', margin: '10px' }} />
+              <img src={`${process.env.PUBLIC_URL}/img/marcas/tapo.jpg`} alt="Tapo" style={{ height: '28px', margin: '10px' }} />
               
             </div>
             <div className="d-flex justify-content-around align-items-center flex-wrap">
-              <img src="/img/marcas/hikvision.png" alt="Cisco" style={{ height: '50px', margin: '10px' }} />
-              <img src="/img/marcas/dahua.png" alt="Huawei" style={{ height: '50px', margin: '10px' }} />
-              <img src="/img/marcas/tplink.png" alt="Fortinet" style={{ height: '50px', margin: '10px' }} />
-              <img src="/img/marcas/tplink.png" alt="Asus" style={{ height: '50px', margin: '10px' }} />
-              <img src="/img/marcas/tplink.png" alt="Linksys" style={{ height: '50px', margin: '10px' }} />
+              <img src={`${process.env.PUBLIC_URL}/img/marcas/cisco.jpg`} alt="Cisco" style={{ height: '45px', margin: '10px' }} />
+              <img src={`${process.env.PUBLIC_URL}/img/marcas/huawei.jpg`} alt="Huawei" style={{ height: '65px', margin: '10px' }} />
+              <img src={`${process.env.PUBLIC_URL}/img/marcas/fortinet.jpg`} alt="Fortinet" style={{ height: '100px', margin: '10px' }} />
+              <img src={`${process.env.PUBLIC_URL}/img/marcas/asus.jpg`} alt="Asus" style={{ height: '45px', margin: '10px' }} />
+              <img src={`${process.env.PUBLIC_URL}/img/marcas/linksys.jpg`} alt="Linksys" style={{ height: '50px', margin: '10px' }} />
               
             </div>
             <p className="text-muted mt-2 w-100 text-center" style={{ fontSize: '0.9rem' }}>
